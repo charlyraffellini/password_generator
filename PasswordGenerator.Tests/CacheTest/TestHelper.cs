0d88bdd4a7746243ca558a415925b23dc04851f7
Charly <charlyraffellini@users.noreply.github.com>
2016-06-09 18:28:00 +0100

    TODO for extract constant of magic number in tests

--- PasswordGenerator.Tests/CacheTest/TestHelper.cs
+++ PasswordGenerator.Tests/CacheTest/TestHelper.cs
@@ -7,7 +7,7 @@ namespace PasswordGenerator.Tests.CacheTest
     {
         public static PasswordCache CreateNewOneSecondExpirationPasswordCache(string instanceName = null)
         {
-            return new PasswordCache(instanceName ?? new Random().Next(10000000).ToString(), 1);
+            return new PasswordCache(instanceName ?? new Random().Next(10000000).ToString(), 1);//TODO this 1 is a magic number. Extract to constant and use in Task.Delay() in tests 
         }
 
         public static Email _defaultEMail = new Email("email@email.com");
